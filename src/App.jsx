2e9feb17fdb3bb1295eb6e3fe750fac59047f764
Areli Rangel <sheccidzavala94@gmail.com>
2023-08-14 17:16:37 -0600

    counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
 
-
+import {useState}  from "react"
 import './App.css'
 
 function App() {
-
+  const [counter, setCounter] = useState(1)
+const suma = ( ) => {
+setCounter(counter + 1 )
+}
+const resta = () => {
+  setCounter(counter -1)
+}
+const reset = () => {
+  setCounter(0)
+}
 
   return (
     <>
-  <h1>main</h1>
+    
+  <h1>{counter}</h1>
+  <button onClick={suma}>+</button>
+  <button onClick={resta}>-</button>
+  <button onClick={reset}>reset</button>
     </>
   )
 }
 
 export default App
+
+// git init = inicia el repositorio 
+// git add . = 
+// git commit -m "nombre"
+// git branch = te muestra el menú de las ramas creadas 
+// git checkout -b nombre rama = crear una nueva rama 
+// git checkout nombre rama existente = para moverse entre ramas 
